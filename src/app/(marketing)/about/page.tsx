86814898da7494671f2979e9e3f0be093d910cd1
lahirujay99 <lahirujayawardana94@gmail.com>
2025-01-20 07:57:24 +0530

    added metadata section and dynamic metadata section

--- src/app/(marketing)/about/page.tsx
+++ src/app/(marketing)/about/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About Code Evolution",
+};
+
 export default function About() {
   return <h1>About</h1>;
 }
--- src/app/(marketing)/product/[productId]/page.tsx
+++ src/app/(marketing)/product/[productId]/page.tsx
@@ -1,8 +1,24 @@
-export default async function ProductItem({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: Promise<{ productId: string }>;
-}) {
+};
+
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const { productId } = await params;
+  const productName = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iphone ${productId}`);
+    }, 100);
+  });
+  return {
+    title: `product ${productName}`,
+  };
+};
+
+export default async function ProductItem({ params }: Props) {
   const { productId } = await params;
   return <h1>Product ID is {productId}</h1>;
 }
