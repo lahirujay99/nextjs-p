ea657ab6bd560fc34ac6bdf1cbb5db01777274ba
lahirujay99 <lahirujayawardana94@gmail.com>
2025-01-20 12:35:06 +0530

    use advanced title manipulate parameters using metadata

--- src/app/(marketing)/blog/page.tsx
+++ src/app/(marketing)/blog/page.tsx
@@ -1,3 +1,12 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  // title: "hm kohomada", template eke thiyena value ekath ekka ekathu wela meka pennananwa title eke
+  title: {
+    absolute: "me thama aththama title eka", // absolute title eka danne mehema ethakota ara default, template override karala meka thama pennanne
+  },
+};
+
 export default function Blog() {
   return <h1>Blog</h1>;
 }
