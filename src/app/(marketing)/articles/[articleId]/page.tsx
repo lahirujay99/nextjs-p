020cc7b6dda47722f7c58d73187cff761040b95f
lahirujay99 <lahirujayawardana94@gmail.com>
2025-01-22 09:38:15 +0530

    experiment with use-hook in react

--- src/app/(marketing)/articles/[articleId]/page.tsx
+++ src/app/(marketing)/articles/[articleId]/page.tsx
@@ -1,14 +1,39 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-export default async function Articles({
+// export default async function Articles({
+//   params,
+//   searchParams,
+// }: {
+//   params: Promise<{ articleId: string }>;
+//   searchParams: Promise<{ lang?: "en" | "es" | "fr" }>;
+// }) {
+//   const { articleId } = await params;
+//   const { lang = "en" } = await searchParams;
+//   return (
+//     <div>
+//       <h1>News Article is {articleId}</h1>
+//       <p>Reading in Language is {lang}</p>
+//       <div className="flex flex-row gap-5 text-lg bg-cyan-800 text-white py-5 pl-3 mt-5">
+//         <Link href={`/articles/${articleId}?lang=en`}>Englsh</Link>
+//         <Link href={`/articles/${articleId}?lang=es`}>Spanish</Link>
+//         <Link href={`/articles/${articleId}?lang=fr`}>French</Link>
+//       </div>
+//     </div>
+//   );
+// }
+"use client";
+import Link from "next/link";
+import { use } from "react";
+
+export default function Articles({
   params,
   searchParams,
 }: {
   params: Promise<{ articleId: string }>;
   searchParams: Promise<{ lang?: "en" | "es" | "fr" }>;
 }) {
-  const { articleId } = await params;
-  const { lang = "en" } = await searchParams;
+  const { articleId } = use(params);
+  const { lang = "en" } = use(searchParams);
   return (
     <div>
       <h1>News Article is {articleId}</h1>
