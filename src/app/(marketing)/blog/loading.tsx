da552e2e85d082dfa2a791d909c8944226af971b
lahirujay99 <lahirujayawardana94@gmail.com>
2025-01-23 16:17:25 +0530

    added loading ui

---
+++ src/app/(marketing)/blog/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return <div>Loading</div>;
+}
+
+//load wenawa wge wadkata onenan meka use karanna puluwan.
+//loading.tsx file eka add karala onenan eka customise karanna puluwan ekko skeleton ekak nttn
+// spinner ekak mokak hari kamathi ekak mekata dagena customise karaganna thiyenne
