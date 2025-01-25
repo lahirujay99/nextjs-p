7cf08806ac1bff95dc61e5a47c9a5e1991dec4d2
lahirujay99 <lahirujayawardana94@gmail.com>
2025-01-26 00:51:04 +0530

    added custom error handling logic

--- src/app/(marketing)/product/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/(marketing)/product/[productId]/reviews/[reviewId]/page.tsx
@@ -1,10 +1,19 @@
 import { notFound, redirect } from "next/navigation";
+
+function getRandomInt(count: number) {
+  return Math.floor(Math.random() * count);
+}
 export default async function ReviewDetail({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) {
   const { reviewId, productId } = await params;
+  const countedNumber = getRandomInt(2);
+  console.log(countedNumber);
+  if (countedNumber === 1) {
+    throw new Error("Error in handling Reviews");
+  }
   if (parseInt(reviewId) > 1000) {
     notFound();
     //redirect("/product"); // mekenuth karanne router.push() wagema wadak
