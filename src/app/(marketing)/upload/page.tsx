8787c010365daf36f59ec8dbd3817ee269076013
lahirujay99 <lahirujayawardana94@gmail.com>
2025-01-29 23:16:40 +0530

    small change in upload component

--- src/app/(marketing)/upload/page.tsx
+++ src/app/(marketing)/upload/page.tsx
@@ -2,13 +2,13 @@ import { onSubmit } from "@/app/actions";
 
 export default function page() {
   return (
-    <div className="flex flex-col bg-cyan-300 py-5 pl-5">
-      <form action={onSubmit}>
+    <div className=" bg-cyan-300 py-5 pl-5">
+      <form action={onSubmit} className="flex flex-col max-w-lg gap-5">
         <input type="file" name="file" />
         <input
           type="submit"
           value="upload"
-          className="bg-cyan-800 rounded text-white font-medium p-2"
+          className="bg-cyan-800 rounded text-white font-medium p-2 w-1/4"
         />
       </form>
     </div>
